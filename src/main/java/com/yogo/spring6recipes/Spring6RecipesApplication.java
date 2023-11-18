eecbb269dbb3d9abd3cb70ac33107602b2a3dcc3
yogo254 <yogo254@yahoo.com>
2023-11-18 13:50:57 +0300

    initial commit

---
+++ src/main/java/com/yogo/spring6recipes/Spring6RecipesApplication.java
@@ -0,0 +1,13 @@
+package com.yogo.spring6recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6RecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6RecipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yogo/spring6recipes/Spring6RecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogo.spring6recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6RecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
